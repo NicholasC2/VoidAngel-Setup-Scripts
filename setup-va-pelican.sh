cad1c360e70cb30c94bc3b56b9446fec850d9ee7
NicholasC2 <nicholasc2@proton.me>
2026-07-09 19:07:57 +1200

    fixed docker setup

--- setup-va-pelican.sh
+++ setup-va-pelican.sh
@@ -3,13 +3,26 @@
 BASE_DIR="/usr/share"
 
 setupDocker() {
-    if ! command -v docker >/dev/null 2>&1; then
-        echo "Docker is not installed. Installing..."
+    if ! command -v docker >/dev/null 2>&1 || ! docker compose version >/dev/null 2>&1; then
+        echo "Docker or Docker Compose plugin not found. Installing/Repairing..."
 
         sudo apt-get update
-        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        sudo apt-get install -y ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        
+        sudo apt-get update
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        
+        echo "Docker and Compose plugin installed successfully."
     else
-        echo "Docker is installed."
+        echo "Docker and Docker Compose plugin are already installed and functional."
     fi
 }
 
